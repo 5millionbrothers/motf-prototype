837226aabeb498f2ac81c682c456bcdd018ee6ac
5millionbrothers <5millionbrothers@gmail.com>
2026-06-20 16:52:31 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 window.MOTF_CONFIG = {
   NAVER_MAP_KEY_ID: "",
   TOSS_CLIENT_KEY: "test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm",
+  SUPABASE_URL: "https://izbwcqnvwsdijoognoag.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_dPivJzpG1ZQWNyTupQ4HRw_dbo5sl9X",
 };
