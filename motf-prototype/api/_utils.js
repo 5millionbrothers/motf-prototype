7d02803454fb52fe1262ba9a7f43f6da349fa5d3
5millionbrothers <5millionbrothers@gmail.com>
2026-07-09 02:52:01 +0900

    Delete motf-prototype directory

--- motf-prototype/api/_utils.js
+++
@@ -1,7 +0,0 @@
-function json(res, statusCode, body) {
-  res.statusCode = statusCode;
-  res.setHeader("Content-Type", "application/json; charset=utf-8");
-  res.end(JSON.stringify(body));
-}
-
-module.exports = { json };
--- motf-prototype/config.js
+++
@@ -1,7 +0,0 @@
-window.MOTF_CONFIG = {
-  NAVER_MAP_KEY_ID: "",
-  PORTONE_STORE_ID: "",
-  PORTONE_CHANNEL_KEY: "",
-  SUPABASE_URL: "https://izbwcqnvwsdijoognoag.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_dPivJzpG1ZQWNyTupQ4HRw_dbo5sl9X",
-};
